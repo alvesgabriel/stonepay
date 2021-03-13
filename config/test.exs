5c0e69e9a2fe941fb2c0f8812bb26e3e01a461fa
Gabriel Alves <gabriel.alves.monteiro1@gmail.com>
2021-03-13 12:12:04 -0300

    :construction_worker: Configured Github Actions to application
    close #2

--- config/test.exs
+++ config/test.exs
@@ -9,7 +9,7 @@ config :stonepay, Stonepay.Repo,
   username: "postgres",
   password: "postgres",
   database: "stonepay_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
+  hostname: System.get_env("DB_HOSTNAME") || "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
